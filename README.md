48ad4b9476a99a2f19bc06b7423c9c04a5c1843d
Kai Lyons <kai@potabi.com>
2022-04-21 10:06:15 -0600

    Allow use of .monster files, and generate proper HTML

--- README.md
+++ README.md
@@ -3,8 +3,20 @@
     <h3>Monster Markup Generator</h3>
 </div>
 
-An experimental generator for the Monster development kit. This is
+An experimental generator for the Monster markup generator. This is
 not a complete project, and there is still plenty wrong with it but
 it is so far just a basic proof of concept.
 
-All it does is work with basic html files. It is not advanced
+## How to use it:
+* Install the V programming language compiler
+* Open a terminal in the `test/` directory
+* run `v run ../src/monster.v index.monster index.html`
+
+## Files:
+* `*.mc` - Monster componant files, these are read by the MonsterFile
+* `MonsterFile` - Combines and names all of the components
+* `*.monster` - The page file you have made
+
+## Why?
+I made Monster because I wanted to make HTML components so I can work with
+TailwindCSS without my initial code being an eye sore. 
--- src/monster.v
+++ src/monster.v
@@ -31,15 +31,47 @@ fn main(){
 	mut tag_output := []string{}
 	mut open_files := []string{}
 	for i in files {
-		open_files << vfr.read_lines(i)
+		open_files << vfr.read(i)
 	}
 	for i in open_files{
 		tag_output << i
 	}
-	output := os.args[1]
+	input := os.args[1]
+	input_file := vfr.read_lines(input)
+	output := os.args[2]
 	mut data_to_write := []byte{}
-	for i in tag_output{
-		data_to_write << i.bytes()
+	for i in input_file{
+		mut tag := ""
+		for char in i {
+			if rune(char).str() != " " {
+				tag += rune(char).str()
+			}
+		}
+		monster_call := rune(tag[1]).str()
+		if monster_call == "!" {
+			mut calls := ""
+			for char in tag {
+				mut c := rune(char).str()
+				if c != "<" && c != "!" && c != ">"{
+					calls += c
+				}
+			}
+			mut pos := -1
+			for item in tags {
+				pos += 1
+				if calls == item {
+					for char in tag_output[pos] {
+						if rune(char).str() != "\n"{
+							data_to_write << char
+						}
+					}
+				}
+			}
+		} else {
+			for char in tag {
+				data_to_write << char
+			}
+		}
 	}
 	export := os.open_file(output, "w") ?
 	mut export_data := io.new_multi_writer(export)
